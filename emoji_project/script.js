13ec7a78886af18319931a2d7c5e89c6b2ca517e
ashley6dec <ashley6dec@gmail.com>
2024-01-17 23:18:59 +0530

    file added

--- emoji_project/script.js
+++ emoji_project/script.js
@@ -22,27 +22,45 @@ function displayResult(searchQuery=""){
    parent.innerHTML=""; //to clear data to display ans
 
     filteredData.forEach((e)=>{   //emojiList is directly from emoji.js file
-        let newRow=document.createElement("tr");
-        let new_Emoji = document.createElement("td");
-        let new_aliases = document.createElement("td");
-        let new_desc = document.createElement("td");
-       
-       new_Emoji.innerText = e.emoji;  //object fetching from emoji.js file
-       new_aliases.innerText = e.aliases;
-       new_desc.innerText = e.description;
-       
-        // new_Emoji.classList.add("emoji");
-        // new_aliases.classList.add("aliases");
-        // new_desc.classList.add("description");
+
+
         
-      newRow.appendChild(new_Emoji);
-      newRow.appendChild(new_aliases);
-      newRow.appendChild(new_desc);
+            let div = document.createElement("div");
+            div.innerHTML = `
+                    <p class="emoji">${e.emoji}</p>
+                    <p class="alias">${e.aliases[0]}</p>
+                    <p class="description">${e.description}</p>
+                `;
+            div.classList.add("emoji-card");
+           parent.appendChild(div);
+          });
+        
+
+
+
+
+
+    //     let newRow=document.createElement("tr");
+    //     let new_Emoji = document.createElement("td");
+    //     let new_aliases = document.createElement("td");
+    //     let new_desc = document.createElement("td");
+       
+    //    new_Emoji.innerText = e.emoji;  //object fetching from emoji.js file
+    //    new_aliases.innerText = e.aliases;
+    //    new_desc.innerText = e.description;
+       
+    //     // new_Emoji.classList.add("emoji");
+    //     // new_aliases.classList.add("aliases");
+    //     // new_desc.classList.add("description");
+        
+    //   newRow.appendChild(new_Emoji);
+    //   newRow.appendChild(new_aliases);
+    //   newRow.appendChild(new_desc);
       
 
-      parent.appendChild(newRow);
+    //   parent.appendChild(newRow);
 
-    })
+    // })
 
 }
 
